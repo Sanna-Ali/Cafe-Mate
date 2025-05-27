eceec8c5e69c563081193a73fa9e757cfc44268b
Sanna-Ali <133492348+Sanna-Ali@users.noreply.github.com>
2025-05-27 21:24:10 +0300

    Update dashboard.js

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,6 +3,6 @@ const connection = require("../config/DB");
 const router = express.Router();
 const { authenticateToken } = require("../services/authentication");
 const { checkRole } = require("../services/checkRole");
-const { getDetails } = require("../controllers/dashbord");
+const { getDetails } = require("../controllers/dashbord.controller.js");
 router.get("/details", authenticateToken, checkRole, getDetails);
 module.exports = router;
