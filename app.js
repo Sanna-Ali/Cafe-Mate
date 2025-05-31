5bca14922dd1d54463a5a846798ef91103645935
Sanna-Ali <133492348+Sanna-Ali@users.noreply.github.com>
2025-05-31 20:54:36 +0300

    Update app.js

--- app.js
+++ app.js
@@ -29,31 +29,3 @@ app.use("/product", productRouter);
 app.use("/bill", billRoute);
 app.use("/dashpord", dashpord);
 module.exports = app;
-
-///////////////////////////3/2
-//
-// const staticImages = express.static(path.join(path.resolve(), "files")); //pdf true
-////////////////////////////////
-// routes
-// const google = require("./routes/authgoogleRoute");
-// const auth = require("./routes/auth");
-// const admin = require("./routes/admin");
-// const account = require("./routes/accountManagement");
-// const password = require("./routes/password");
-// const initializePassport = require("./config/passportConfig");
-// const photoUpload = require("./middlewares/photoUpload");
-
-// const i18next = require("i18next");
-//routes
-// const staticFiles = express.static(path.join(path.resolve(), "files"));
-// const staticImages = express.static(path.join(path.resolve(), "images"));
-// app.use("/images", staticImages);
-// app.use("/files", staticFiles);
-// app.use("/api/auth", auth);
-// app.use("/api/admin", admin);
-// app.use("/api/account", account);
-// app.use("/api/password", password);
-// app.use("/api/g", google);
-// // Error Handler Middleware
-// app.use(notFound);
-// app.use(errorHandler);
